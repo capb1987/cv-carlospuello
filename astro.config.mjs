82cdbc4a5f5c5bd73ce0c8703131776180b4597c
Carlos Puello <carlos.puellobolano@gmail.com>
2024-03-09 11:38:42 -0500

    Cambios en Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,20 @@ import react from "@astrojs/react";
 import million from "million/compiler";
 import tailwind from "@astrojs/tailwind";
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
   vite: {
-    plugins: [million.vite({ mode: "react", server: true, auto: true })],
+    plugins: [
+      million.vite({
+        mode: "react",
+        server: true,
+        auto: true,
+      }),
+    ],
   },
+  output: "hybrid",
+  adapter: netlify(),
 });
